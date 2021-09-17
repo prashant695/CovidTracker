8379a12402e6df02bdffcdc2428750c8f82b1205
Prashant <prashantagarwal695@gmail.com>
2021-09-17 23:30:48 +0530

    Deploy Commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const path = require("path")
 const app = express();
-require('dotenv').config()
+require("dotenv").config()
 const port = process.env.PORT || 80;
+
+// console.log(process.env.API_KEY);
 
 app.use('/static', express.static('static'));
 app.use(express.urlencoded());
@@ -13,7 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 app.get("/", (req, res) => {
-    res.status(200).render('index.html')
+    // console.log(process.env.API_KEY)
+    res.status(200).render('index.ejs', {"api_key": process.env.API_KEY})
 });
 app.listen(port, () => {
     console.log(`The application started successfully at ${port}`);
